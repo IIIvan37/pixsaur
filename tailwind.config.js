23a86069a969cb4cb1750677e10a2e2034edf773
Ivan Duchauffour <ivanduchauffour@MBP-de-Ivan.lan>
2025-05-16 20:31:30 +0200

    remove tailwindcss again

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-export default{
-    content: ["./src/**/*.{html,js}"], //you can also add jsx and any other file types that you need to style with tailwind
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
