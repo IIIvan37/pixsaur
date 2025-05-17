ff87e97da4cfd74ff9435711d0244154894778d7
IDuchauffour <iduchauffour@gmail.com>
2025-05-17 12:08:05 +0200

    handle svg files
    starting to tidy up file hierarchy

--- src/components/image-upload/image-upload.tsx
+++ src/components/image-upload/image-upload.tsx
@@ -6,18 +6,48 @@ import { UploadIcon } from '@radix-ui/react-icons'
 export type ImageUploadProps = {
   onImageLoaded: (img: HTMLImageElement) => void
 }
+
+/**
+ * Handles loading an image file and processing it.
+ *
+ * @param {File} file - The image file to process.
+ * @param {(img: HTMLImageElement) => void} onImageLoaded - Callback to handle the loaded image.
+ */
+const processImageFile = async (
+  file: File,
+  onImageLoaded: (img: HTMLImageElement) => void
+) => {
+  const url = URL.createObjectURL(file)
+  const img = new Image()
+  img.src = url
+
+  img.onload = async () => {
+    if (file.type === 'image/svg+xml') {
+      const container = document.createElement('div')
+      container.innerHTML = await file.text()
+      const svg = container.querySelector('svg')
+
+      if (svg) {
+        const viewBox = svg.getAttribute('viewBox')
+        if (viewBox) {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const [_x, _y, width, height] = viewBox.split(/\s+|,/)
+          img.width = parseInt(width)
+          img.height = parseInt(height)
+        }
+      }
+    }
+    onImageLoaded(img)
+    URL.revokeObjectURL(url)
+  }
+}
+
 export const ImageUpload = memo(({ onImageLoaded }: ImageUploadProps) => {
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
       const file = acceptedFiles[0]
       if (file && file.type.startsWith('image/')) {
-        const url = URL.createObjectURL(file)
-        const img = new Image()
-        img.src = url
-        img.onload = () => {
-          onImageLoaded(img)
-          URL.revokeObjectURL(url)
-        }
+        processImageFile(file, onImageLoaded)
       }
     },
     [onImageLoaded]
