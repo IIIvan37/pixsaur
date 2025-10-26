896344c8c56a5e8080a13c500f33794f07117189
IIIvan37 <ivan.duchauffour@gmail.com>
2025-10-26 23:58:44 +0100

    chore: remove unused dsk library
    - Remove src/libs/dsk/ directory (add-file.ts, create-dsk.ts, test.ts)
    - Unused DSK image format utilities

--- src/libs/dsk/test.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs/promises'
-
-import { createBlankDSK } from './create-dsk.ts'
-
-const blankDsk = createBlankDSK('PIXSAUR')
-
-// dskWithFiles now contains both files, AMSDOS-compatible, with extents if needed
-await fs.writeFile('disk.dsk', blankDsk, { encoding: null })
