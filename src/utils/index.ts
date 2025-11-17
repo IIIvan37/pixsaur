26e46d673bf0eea7f33f3985900ae737f3f0d501
Ivan Duchauffour <iduchauffour@welcomr.com>
2025-11-17 20:56:57 +0100

    refactor(utils): add central utils index to ease migration

---
+++ src/utils/index.ts
@@ -0,0 +1,34 @@
+// Centralized re-exports for utils to ease refactor and migration.
+// Move domain-specific utilities into subfolders (utils/core, utils/image, utils/exports, utils/cpc, utils/platform, utils/test)
+// and update these re-exports progressively to keep backwards compatibility during refactor.
+
+// specific utilities
+export * from './amsdos-filename'
+// cpc / export helpers
+export * from './cpc-calculations'
+export * from './download-file'
+export * from './exports/asm-templates'
+export * from './exports/color-utils'
+export * from './exports/cpc-plus-format'
+export * from './exports/export-tauri'
+export * from './exports/export-zip'
+export * from './exports/exporters/export-linear'
+export * from './exports/exporters/export-palette'
+export * from './exports/exporters/export-png'
+export * from './exports/exporters/export-scr'
+export * from './exports/generate-dsk-readme'
+export * from './exports/to-asm-data'
+export * from './get-visual-region'
+export * from './image-processing/horizontal-smoothing'
+// image helpers
+export * from './image-resize'
+export * from './invariant'
+export * from './is-development'
+export * from './is-tauri'
+export * from './logger'
+export * from './quit-shortcut'
+
+// testing
+export * from './test-utils'
+export * from './validate-custom-dimensions'
+export * from './validate-custom-dimensions'
