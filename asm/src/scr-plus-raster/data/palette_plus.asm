ad6383be78f6ebeced5f8eb8f3e56fbc8294ab42
Ivan Duchauffour <iduchauffour@welcomr.com>
2025-12-17 23:09:36 +0100

    feat(asm): add example data files for demo programs
    - Add sample exported image data and palettes
    - Add raster data examples for CPC Classic and CPC Plus
    - Add expanded source files for reference

---
+++ asm/src/scr-plus-raster/data/palette_plus.asm
@@ -0,0 +1,6 @@
+; Palette Data created with Pixsaur - CPC+
+; Mode 0  
+; 160x200 pixels, 80x200 bytes.
+
+Palette:
+    DEFW #0000, #0000, #0000, #0000, #0161, #0001, #0021, #0011, #0FFF, #0FF6, #0C9E, #0FBE, #0100, #0000, #0112, #0151
